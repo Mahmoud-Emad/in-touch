1aa1284fcc9687acfd7de97487345bfb0da4e0f9
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-08-21 15:03:50 +0200

    Add mute, unmute, playVideo, stopVideo functions.

--- public/script.js
+++ public/script.js
@@ -79,3 +79,57 @@ const scrollToBottom = () => {
 }
 
 
+const muteUnmute = () => {
+    const enabled = videoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        videoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else {
+        setMuteButton();
+        videoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const playStop = () => {
+    const enabled = videoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        videoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo();
+    } else {
+        setStopVideo();
+        videoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+const setMuteButton = () => {
+    const html = `
+        <i class="fas fa-microphone"></i>
+        <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+    const html = `
+        <i class="unmute fas fa-microphone-slash"></i>
+        <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setStopVideo = () => {
+    const html = `
+      <i class="fas fa-video"></i>
+      <span>Stop Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+  }
+  
+  const setPlayVideo = () => {
+    const html = `
+    <i class="stop fas fa-video-slash"></i>
+      <span>Play Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+  }
+
